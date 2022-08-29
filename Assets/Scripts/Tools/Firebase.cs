6a8fc8fc4ec47e3a123da339beb5a523ed3dac84
EliasVal <46247153+EliasVal@users.noreply.github.com>
2022-08-29 12:39:42 +0300

    backend

--- Assets/Scripts/Tools/Firebase.cs
+++ Assets/Scripts/Tools/Firebase.cs
@@ -105,8 +105,9 @@ public class Firebase : MonoBehaviour
     NameValueCollection data = new NameValueCollection();
 
     data["code"] = code;
+    webClient.Headers["User-Agent"] = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MDDC)";
 
-    webClient.UploadValuesTaskAsync(new System.Uri(testMode ? "http://localhost:3000/keepLobbyAlive" : "https://JooBot.eliasval.repl.co/keepLobbyAlive"), "POST", data);
+    webClient.UploadValuesTaskAsync(new System.Uri(testMode ? "http://localhost:3000/keepLobbyAlive" : "https://joobot.glitch.me/keepLobbyAlive"), "POST", data);
   }
 
   public struct Response
