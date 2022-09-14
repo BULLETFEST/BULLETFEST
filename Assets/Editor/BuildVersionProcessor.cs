a5a9d5fbd5b5853b929604ad2977f6f803815386
EliasVal <46247153+EliasVal@users.noreply.github.com>
2022-09-14 13:55:53 +0300

    Trying IL2CPP, simplified workflow

--- Assets/Editor/BuildVersionProcessor.cs
+++ Assets/Editor/BuildVersionProcessor.cs
@@ -16,6 +16,19 @@ public class BuildVersionProcessor : IPreprocessBuildWithReport
     PlayerSettings.bundleVersion = version.GetVersionString();
     PlayerSettings.SplashScreen.show = false;
 
+    BuildTargetGroup bt = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+
+#if UNITY_EDITOR_LINUX
+    PlayerSettings.SetScriptingBackend(bt, ScriptingImplementation.IL2CPP);
+    PlayerSettings.SetIl2CppCompilerConfiguration(bt, Il2CppCompilerConfiguration.Master);
+    PlayerSettings.SetManagedStrippingLevel(bt, ManagedStrippingLevel.High);
+    EditorUserBuildSettings.il2CppCodeGeneration = Il2CppCodeGeneration.OptimizeSpeed;
+#elif UNITY_EDITOR_WIN
+    PlayerSettings.SetScriptingBackend(bt, ScriptingImplementation.Mono2x);
+    PlayerSettings.SetIl2CppCompilerConfiguration(bt, Il2CppCompilerConfiguration.Master);
+    PlayerSettings.SetManagedStrippingLevel(bt, ManagedStrippingLevel.Disabled);
+#endif
+
 #if UNITY_STANDALONE_OSX
     UnityEditor.OSXStandalone.UserBuildSettings.architecture = UnityEditor.OSXStandalone.MacOSArchitecture.x64ARM64;
 #endif
